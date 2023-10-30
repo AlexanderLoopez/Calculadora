57f8ceff0241aceaa9fa6c01932a9e56a1e0e384
Alex <alexanderls212@gmail.com>
2023-10-30 16:59:21 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.jpg';
+import Boton from './componentes/Boton';
+
+function App() {
+  return (
+    <div className='App'>
+     <div className='freecodecamp-logo-contenedor'>
+      <img 
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='Logo de freeCodeCamp' />
+     </div>
+     <div className='contenedor-calculadora'>
+      <div className='fila'></div>
+        <Boton>1</Boton>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>  
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Boton(props) {
+  return (
+    <div
+    className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Boton;
